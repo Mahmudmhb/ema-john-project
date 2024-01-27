94f03b3f39f19c2f83c305a5590a541117103d7a
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-01-27 11:36:25 +0600

    cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,20 +9,20 @@ const Cart = (props) => {
     let subTotal = 0;
     let quantity = 0;
     for(const product of props.cart){
-        if(product.quantity === 0){
-            product.quantity = 1;
-        }
-        
+        product.quantity = product.quantity || 1;
+      
          total = total + product.price * product.quantity;
-        shipingTotal = shipingTotal + product.shipping * product.quantity;
+        shipingTotal = shipingTotal + product.shipping ;
+        // quantity = quantity + product.quantity;
         quantity = quantity + product.quantity;
+        console.log(quantity)
     }
     const tex = total * 7 /100;
     subTotal= total + shipingTotal + tex;
     return (
         <div className='cart-container'>
              <h3>Order Summary</h3>
-            <h4>Selected Items:{props.cart.length}</h4>
+            <h4>Selected Items:{quantity}</h4>
             <h4>Total Price: ${total}</h4>
             <h4>Total Shipping Charge: ${shipingTotal}</h4>
             <h4>Tax: ${tex .toFixed(2)}  </h4>
